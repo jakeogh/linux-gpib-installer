069e3b04f51bff96add4327497a5c8d7d40b9de4
jakeogh <github.com@v6y.net>
2022-10-07 14:31:05 -0700

    remove fastentrypoints

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 from setuptools import find_packages
 from setuptools import setup
 
-import fastentrypoints
+# import fastentrypoints
 
 if not sys.version_info[0] == 3:
     sys.exit("Python 3 is required. Use: 'python3 setup.py install'")
@@ -27,7 +27,6 @@ config = {
     "description": "installer for GPIB drivers",
     "long_description": __doc__,
     "packages": find_packages(exclude=["tests"]),
-    "package_data": {},
     "include_package_data": True,
     "zip_safe": False,
     "platforms": "any",
